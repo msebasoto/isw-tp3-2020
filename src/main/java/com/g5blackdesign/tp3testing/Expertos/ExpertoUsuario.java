c80163dcb023f76c27540f2277e022cb8ddfa8fc
nachogg18 <44012745+nachogg18@users.noreply.github.com>
2020-06-11 18:19:05 -0300

    Modificado Experto de Usuario
    se valida nombre de usuario

--- src/main/java/com/g5blackdesign/tp3testing/Expertos/ExpertoUsuario.java
+++ src/main/java/com/g5blackdesign/tp3testing/Expertos/ExpertoUsuario.java
@@ -16,9 +16,20 @@ public class ExpertoUsuario {
     private Usuario usuario;
     public Carrito carrito;
     
+    public static boolean contieneSoloLetras(String cadena) {
+        for (int x = 0; x < cadena.length(); x++) {
+            char c = cadena.charAt(x);
+            // Si no está entre a y z, ni entre A y Z, ni es un espacio
+            if (!((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == ' ')) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
     
     public boolean CrearUsuario(Carrito carrito, String nombre, String apellido, String dni, String email){
-        if (!carrito.getListdetallecarrito().isEmpty()) {
+        if (!carrito.getListdetallecarrito().isEmpty() && ExpertoUsuario.contieneSoloLetras(nombre)) {
             usuario = new Usuario(nombre,apellido,dni,email);
             usuario.setCarrito(carrito);
             return true;
