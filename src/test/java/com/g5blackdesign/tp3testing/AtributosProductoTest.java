95c3b1270b60d9223114cf4d5b056788fc98ca1d
Maria Luz <marialuztejada055@gmail.com>
2020-06-11 10:15:37 -0300

    Test arreglados y pom modificado

---
+++ src/test/java/com/g5blackdesign/tp3testing/AtributosProductoTest.java
@@ -0,0 +1,29 @@
+package com.g5blackdesign.tp3testing;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import com.g5blackdesign.tp3testing.Entidades.Producto;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+
+class TestAtributosproducto{
+
+    static Producto producto1=new Producto("comida", "galleta", 45);
+   
+    @Test
+    public void testNombreNoDebeEstarVacio(){
+        assertNotEquals("", producto1.getNombre());
+    }
+
+    @Test
+    public void testPrecioProductoMayorIgualCero() {
+
+        final float precioReal = producto1.getPrecio();
+        assertEquals(true, precioReal>=0);
+        
+   }
+}
