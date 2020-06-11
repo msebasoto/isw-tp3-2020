d8876be4a9893148a0a210bf0432dfd848b974d6
Juan Sebastián Martin <38918218+sebamartin10@users.noreply.github.com>
2020-06-11 12:41:34 -0300

    Inicial

---
+++ CarritoComprasEcomerce/CarritoCompras.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarritoComprasEcomerce
+{
+    public static class CarritoCompras
+    {
+        public static decimal CalcularTotal(List<(int cantidad,decimal precioUnitario)>detalle) {
+            decimal total = 0;
+            foreach (var item in detalle) {
+                total = total + (item.cantidad * item.precioUnitario);
+            }
+            return total;
+        }
+        
+    }
+}
---
+++ LibreriaPruebas/CarritoComprasPruebas.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarritoComprasEcomerce;
+using Xunit;
+
+namespace LibreriaPruebas
+{
+    public class CarritoComprasPruebas
+    {
+        [Fact]
+        public void CarritoComprasPrueba_TotalNoPuedeSerMenorIgualCero() {
+            //float expected = 490;
+
+            List<(int cantidad, decimal precioUnitario)> detalle = new List<(int, decimal)>();
+            detalle.Add((2,30));
+            detalle.Add((1,150));
+            detalle.Add((4,70));
+
+            Assert.True(CarritoCompras.CalcularTotal(detalle)>0);
+        }
+        [Fact]
+        public void CarritoComprasPrueba_TotalDebeRetornarDecimal() {
+            List<(int cantidad, decimal precioUnitario)> detalle = new List<(int, decimal)>();
+            detalle.Add((2, 30));
+            detalle.Add((1, 150));
+            detalle.Add((4, 70));
+
+            var expected = typeof(decimal);
+            var actual = CarritoCompras.CalcularTotal(detalle);
+
+            Assert.IsType<decimal>(actual);
+            Assert.IsType(expected, actual);
+        }
+    }
+}
